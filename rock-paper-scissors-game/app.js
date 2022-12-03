2a6c94d419ad120d48384e5db04bddb922a6911e
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-03 17:14:15 +0300

    Change game HTML CSS and script,write new playRound

--- rock-paper-scissors-game/app.js
+++ rock-paper-scissors-game/app.js
@@ -1,3 +1,65 @@
+let buttons = document.querySelectorAll(".button");
+const optionBtn = document.querySelectorAll("div.optionBtn button");
+const roundResults = document.querySelector("#roundResults");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const resetBtn = document.querySelector("#reset");
+
+//refresh page for new game
+resetBtn.addEventListener("click", () => location.reload());
+
+buttons.forEach((button) => {
+  button.addEventListener("click", getPlayerChoice);
+});
+
+function getPlayerChoice(e) {
+  let computerSelection = "paper";
+  let playerSelection = e.target.id;
+  console.log(playerSelection);
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+      if (computerSelection === "rock") {
+        result = "d";
+      } else if (computerSelection === "paper") {
+        result = "c";
+      } else if (computerSelection === "scissors") {
+        result = "p";
+      }
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        result = "p";
+      } else if (computerSelection === "paper") {
+        result = "d";
+      } else if (computerSelection === "scissors") {
+        result = "c";
+      }
+    } else if (playerSelection === "scissors") {
+      if (computerSelection === "rock") {
+        result = "c";
+      } else if (computerSelection === "paper") {
+        result = "p";
+      } else if (computerSelection === "scissors") {
+        result = "d";
+      }
+    }
+
+    if (result === "c") {
+      alert(`Computer wins, cause Computer selected: ${computerSelection}!`);
+    } else if (result === "d") {
+      alert("It's a draw, Please play again!");
+    } else if (result === "p") {
+      alert(`You win!, couse Computer selected: ${computerSelection}`);
+    }
+    return result;
+  }
+
+  playRound(playerSelection, computerSelection);
+}
+
+//Old script
+/* let playerScore = 0;
+let computerScore = 0;
+
 const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
@@ -9,37 +71,66 @@ function getComputerChoice() {
 
 console.log(computerSelection);
 
-const playerSelection = prompt("Write rock, paper or scissors").toLowerCase();
+let playerSelection = prompt("Write rock, paper or scissors").toLowerCase();
 
-const c = `Computer wins, cause computer selected ${computerSelection}!`;
-const d = "It's a draw, Please play again!";
-const p = `You win!, couse computer selected ${computerSelection}`;
+let result;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      alert(d);
+      result = "d";
     } else if (computerSelection === "paper") {
-      alert(c);
+      result = "c";
     } else if (computerSelection === "scissors") {
-      alert(p);
+      result = "p";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      alert(p);
+      result = "p";
     } else if (computerSelection === "paper") {
-      alert(d);
+      result = "d";
     } else if (computerSelection === "scissors") {
-      alert(c);
+      result = "c";
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      alert(c);
+      result = "c";
     } else if (computerSelection === "paper") {
-      alert(p);
+      result = "p";
     } else if (computerSelection === "scissors") {
-      alert(d);
+      result = "d";
     }
   }
+
+  if (result === "c") {
+    alert(`Computer wins, cause Computer selected: ${computerSelection}!`);
+  } else if (result === "d") {
+    alert("It's a draw, Please play again!");
+  } else if (result === "p") {
+    alert(`You win!, couse Computer selected: ${computerSelection}`);
+  }
+  return result;
 }
+
 playRound(playerSelection, computerSelection);
+
+function playGame() {
+  getComputerChoice();
+  playerSelection;
+  playRound(playerSelection, computerSelection);
+  if (result === "p") {
+    playerScore++;
+  } else if (result === "d") {
+    playerScore = playerScore;
+    computerScore = computerScore;
+  } else if (result === "c") {
+    computerScore++;
+  }
+  return playerScore, computerScore;
+}
+
+for (let j = 0; j < 5; j++) {
+  playGame();
+}
+
+console.log(playerScore, computerScore); */
