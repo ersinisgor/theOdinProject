e0c7ec06e71f06b476e180ae9f1a315cb4d0278b
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-17 16:23:11 +0300

    Change javascript using DOM, add sound

--- rock-paper-scissors-game/app.js
+++ rock-paper-scissors-game/app.js
@@ -1,4 +1,118 @@
+const buttons = document.querySelectorAll(".button");
+const results = document.querySelector("#roundResults");
+const plyScore = document.querySelector("#playerScore");
+const comScore = document.querySelector("#computerScore");
+const resetBtn = document.querySelector("#reset");
 let playerScore = 0;
+let computerScore = 0;
+let writeResult = "";
+
+// for the computer to randomly select weapons
+const computerPlay = function () {
+  const choices = ["rock", "paper", "scissors"];
+  const num = Math.floor(Math.random() * choices.length);
+
+  return choices[num];
+};
+
+const playGame = function () {
+  const playerSelection = this.id;
+  const computerSelection = computerPlay();
+  const result = showResult(playerSelection, computerSelection);
+
+  if (result === "c") {
+    computerScore++;
+    writeResult = `You lose!, ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+  } else if (result === "d") {
+    writeResult = `It's a draw. You both chose ${playerSelection.toUpperCase()}`;
+  } else if (result === "p") {
+    playerScore++;
+    writeResult = `You win!, ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
+  }
+
+  // for count 5 lives and stop game
+  if (playerScore === 5) {
+    writeResult += "<br><br>You won the game! Play Again?";
+    playWinSound();
+    results.setAttribute("style", "background:#86cb92");
+    setTimeout(function () {
+      location.reload();
+    }, 2500);
+  } else if (computerScore === 5) {
+    writeResult += "<br><br>Computer won the game! Play Again?";
+    playLostSound();
+    results.setAttribute("style", "background:#fc5130");
+    setTimeout(function () {
+      location.reload();
+    }, 2500);
+  }
+
+  // to show the results on the screen
+  comScore.innerHTML = computerScore;
+  plyScore.innerHTML = playerScore;
+  results.innerHTML = writeResult;
+  console.log(playerSelection, computerSelection, result);
+};
+
+// game result according to selected values
+const showResult = function (playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      return "d";
+    } else if (computerSelection === "paper") {
+      return "c";
+    } else if (computerSelection === "scissors") {
+      return "p";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "p";
+    } else if (computerSelection === "paper") {
+      return "d";
+    } else if (computerSelection === "scissors") {
+      return "c";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "c";
+    } else if (computerSelection === "paper") {
+      return "p";
+    } else if (computerSelection === "scissors") {
+      return "d";
+    }
+  }
+};
+
+// to play audio on every click
+const playSelectSound = function () {
+  const audio = document.querySelector("#select");
+
+  audio.currentTime = 0;
+  audio.play();
+};
+
+// will play when the player wins
+const playWinSound = function () {
+  const audio = document.querySelector("#win");
+  audio.play();
+};
+
+// will play when the computer wins
+const playLostSound = function () {
+  const audio = document.querySelector("#lost");
+  audio.play();
+};
+
+// things to do on every click
+buttons.forEach((button) => button.addEventListener("click", playGame));
+buttons.forEach((button) => button.addEventListener("click", playSelectSound));
+
+//refresh page for new game
+resetBtn.addEventListener("click", () => location.reload());
+
+/* -----------------------------OLD-SCRIPT------------------------------------ */
+
+/* let playerScore = 0;
 let computerScore = 0;
 const buttons = document.querySelectorAll(".button");
 const resetBtn = document.querySelector("#reset");
@@ -54,7 +168,7 @@ buttons.forEach((button) => {
 });
 
 //refresh page for new game
-resetBtn.addEventListener("click", () => location.reload());
+resetBtn.addEventListener("click", () => location.reload()); */
 
 /* ----------------------------------------------------------------------------- */
 
