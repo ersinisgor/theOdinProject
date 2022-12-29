2f038cf5a34b88d71a7ecdbe2c6aa78a368a1f66
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-29 10:16:13 +0300

    Update showScreen

--- 04-Calculator/app.js
+++ 04-Calculator/app.js
@@ -1,4 +1,7 @@
 const nums = document.querySelectorAll(".nums");
+const add = document.querySelector(".add");
+const operator = document.querySelectorAll(".opts");
+const result = document.querySelector(".result");
 let output = "";
 
 /* const getValue = function (e) {
@@ -7,12 +10,22 @@ let output = "";
    e.target.id;
 }; */
 
-const showScreen = function () {
-  const result = document.querySelector(".result");
+/* const showScreen = function () {
   output += this.id;
   result.textContent = output;
+  output = Number(output);
   return output;
+}; */
+
+const showScreen = function (e) {
+  output += e.target.id;
+  result.textContent = output;
+  console.log(output);
 };
+
+nums.forEach((num) => {
+  num.addEventListener("click", showScreen);
+});
 
 /* const input = function (e) {
   Number(e.target.id);
@@ -22,25 +35,54 @@ const showScreen = function () {
   num.addEventListener("click", getValue);
 }); */
 
-nums.forEach((num) => {
-  num.addEventListener("click", showScreen);
+/* Math Functions */
+const addNumbers = function (num1, num2) {
+  output = "";
+  num1 = output;
+  console.log("this is adding");
+  console.log(num1 + num2);
+  /* return num1 + num2; */
+};
+
+const subsNumbers = function (num1, num2) {
+  return num1 - num2;
+};
+
+const divideNumbers = function (num1, num2) {
+  return num1 / num2;
+};
+
+const multiplyNumbers = function (num1, num2) {
+  return num1 * num2;
+};
+
+/* const operate = function (num1, num2, operator) {
+    switch (operator) {
+        case add:addNumbers(num1,num2)   
+            break;
+        case 
+        default:
+            break;
+    }
+}; */
+
+const operate = function () {
+  showScreen();
+  const key = this.id;
+  if (key === "divide") {
+    divideNumbers(5, 10);
+  } else if (key === "multiply") {
+    multiplyNumbers(5, 10);
+  } else if (key === "substract") {
+    subsNumbers(output, 10);
+  } else if (key === "add") {
+    addNumbers(5, 10);
+  }
+};
+
+operator.forEach((opt) => {
+  opt.addEventListener("click", operate);
 });
 
-/* Math Functions */
-const add = function (a, b) {
-  return a + b;
-};
-
-const subs = function (a, b) {
-  return a - b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const operate = function (a, b, operator) {};
+/* Try Add function */
+/* add.addEventListener("click", addNumbers); */
