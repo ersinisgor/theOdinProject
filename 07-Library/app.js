f4c7159a8874b7e221063ba2dbb5a4b6e0db0514
Ersin İşgör <ersinisgor@hotmail.com>
2023-03-02 23:06:37 +0300

    Add CSS for input-form

--- 07-Library/app.js
+++ 07-Library/app.js
@@ -1,3 +1,25 @@
+const addBook = document.querySelector('button[type="submit"]');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+
+addBook.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    closeModal();
+  }
+}
+
 let myLibrary = [];
 
 function Book() {
