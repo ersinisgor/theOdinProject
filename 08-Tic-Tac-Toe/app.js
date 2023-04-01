533e3e9cb1f05bdc7916642402c1c21c6bde4032
Ersin İşgör <ersinisgor@hotmail.com>
2023-04-01 23:02:20 +0300

    Initial commit

---
+++ 08-Tic-Tac-Toe/app.js
@@ -0,0 +1,7 @@
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+
+const startCells = ['', '', '', '', '', '', '', '', ''];
+
+let go = 'circle';
+infoDisplay.textContent = 'Circle goes first';
