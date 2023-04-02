6d7ed587fa0f8b1d2f40d8371584758365dbe0ab
Ersin İşgör <ersinisgor@hotmail.com>
2023-04-02 23:44:43 +0300

    Add CSS

--- 08-Tic-Tac-Toe/app.js
+++ 08-Tic-Tac-Toe/app.js
@@ -1,7 +1,21 @@
 const gameBoard = document.querySelector('#gameboard');
 const infoDisplay = document.querySelector('#info');
-
+const allCells = document.querySelectorAll('.square');
+const restartBtn = document.querySelector('#restartBtn');
 const startCells = ['', '', '', '', '', '', '', '', ''];
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-let go = 'circle';
-infoDisplay.textContent = 'Circle goes first';
+let currentPlayer = 'X';
+
+infoDisplay.textContent = 'Cross goes first';
+
+let running = false;
