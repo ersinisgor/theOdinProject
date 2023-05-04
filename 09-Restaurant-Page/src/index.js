5cab6f251c466bd4de8467df8e5690ccf17266b0
Ersin Isgor <ersinisgor@hotmail.com>
2023-05-04 21:44:14 +0300

    Add image and text

--- 09-Restaurant-Page/src/index.js
+++ 09-Restaurant-Page/src/index.js
@@ -1,1 +1,37 @@
-alert('hello');
+export const initialPage = function () {
+  const content = document.getElementById('content');
+
+  const title = document.createElement('h1');
+  const image = document.createElement('img');
+  const text = document.createElement('p');
+
+  title.innerText = 'How Wonderfull';
+  image.setAttribute('src', '../img/res.jpg');
+  image.style.width = '700px';
+  text.innerText =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, officiis! Quod, autem corrupti repudiandae a recusandae itaque explicabo. Aliquam, debitis alias repellendus vitae blanditiis molestias labore exercitationem? Nobis, iusto eaque.';
+
+  content.appendChild(title);
+  content.appendChild(image);
+  content.appendChild(text);
+};
+
+export const contactPage = function () {
+  const content = document.getElementById('content');
+
+  const title = document.createElement('h1');
+  const image = document.createElement('img');
+  const text = document.createElement('p');
+
+  title.innerText = 'Contact Us';
+  image.setAttribute('src', '../img/cont.jpg');
+  image.style.width = '700px';
+  text.innerText =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, officiis! Quod, autem corrupti repudiandae a recusandae itaque explicabo. Aliquam, debitis alias repellendus vitae blanditiis molestias labore exercitationem? Nobis, iusto eaque.';
+
+  content.appendChild(title);
+  content.appendChild(image);
+  content.appendChild(text);
+};
+
+initialPage();
