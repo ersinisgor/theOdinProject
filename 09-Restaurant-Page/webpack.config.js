70b9c36f2711e19367355db9364dc829f90af5da
Ersin Isgor <ersinisgor@hotmail.com>
2023-05-11 14:16:55 +0300

    UPDATE: webpack entry path

--- 09-Restaurant-Page/webpack.config.js
+++ 09-Restaurant-Page/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/modules/components.js',
+  entry: './src/components.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
